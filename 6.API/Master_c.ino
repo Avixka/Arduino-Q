ec569252d67e3668a96c6d636b06a419ce9686b3
Avishka Dilshan <78788710+Avixka@users.noreply.github.com>
2026-01-16 12:07:47 +0100

    Refactor serial communication to use Serial
    Added timeout handlings.

--- 6.API/Master_c.ino
+++ 6.API/Master_c.ino
@@ -2,39 +2,45 @@
 
 void setup() {
   Bridge.begin();
-  Serial1.begin(115200); 
+  Serial.begin(115200); 
+  
+  Serial.setTimeout(2000); 
+
   Bridge.provide("uart_write", uartWrite);
   Bridge.provide("uart_read", uartRead);
   Bridge.provide("uart_writeread", uartWriteRead); 
 }
 
 void loop() {
- 
+  
 }
 
 void uartWrite(String data) {
-  Serial1.print(data);
+  while(Serial.available()) { Serial.read(); }
+  Serial.print(data);
 }
 
 String uartRead(String ignored) {
-  if (Serial1.available()) {
-    return Serial1.readString();
+  unsigned long startTime = millis();
+  while (Serial.available() == 0) {
+    if (millis() - startTime > 2000) {
+      return "ERROR: TIMEOUT";
+    }
   }
-  return "Nothing recived";
+  return Serial.readString();
 }
 
 String uartWriteRead(String command) {
-  while(Serial1.available()) { Serial1.read(); }
+  while(Serial.available()) { Serial.read(); }
 
-  Serial1.print(command);
+  Serial.print(command);
 
   unsigned long startTime = millis();
-  while (Serial1.available() == 0) {
-    if (millis() - startTime > 1000) {
+  while (Serial.available() == 0) {
+    if (millis() - startTime > 2000) {
       return "ERROR: TIMEOUT";
     }
   }
   
-  String response = Serial1.readString();
-  return response;
+  return Serial.readString();
 }
